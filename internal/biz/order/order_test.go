a22246c343c426b8eb362493f1bc8d654940ca9d
yang.liu14 <yang.liu14@weimob.com>
2022-07-14 23:03:13 +0800

    update mock code

--- internal/biz/order/order_test.go
+++ internal/biz/order/order_test.go
@@ -18,20 +18,23 @@ func TestUsecase_GetOrder(t *testing.T) {
 
 	orderClient := order.NewMockIOrderClient(ctl)
 
-	// input any matcher
-	orderClient.EXPECT().GetOrder(gomock.Any(), gomock.Any()).Return(o, nil)
 	orderUc := NewOrderUsecase(orderClient)
+
+	retErr := errors.New("order: not found")
+	gomock.InOrder(
+		orderClient.EXPECT().GetOrder(gomock.Any(), gomock.Any()).Return(o, nil),
+		orderClient.EXPECT().GetOrder(gomock.Any(), gomock.Eq(uint(1))).Return(o, retErr).AnyTimes(),
+	)
 
 	_order, err := orderUc.GetOrder(context.Background(), uint(1))
 	require.NoError(t, err)
 	require.Equal(t, o, _order)
 
-	// input eq matcher output error
-	var retErr = errors.New("order: not found")
-	orderClient.EXPECT().GetOrder(gomock.Any(), gomock.Eq(uint(1))).Return(o, retErr)
-	orderUc = NewOrderUsecase(orderClient)
+	_order, err = orderUc.GetOrder(context.Background(), uint(1))
+	require.Equal(t, retErr, err)
+	require.Equal(t, o, _order)
 
-	_order, err = orderUc.GetOrder(context.Background(), uint(2))
+	_order, err = orderUc.GetOrder(context.Background(), uint(1))
 	require.Equal(t, retErr, err)
 	require.Equal(t, o, _order)
 }
